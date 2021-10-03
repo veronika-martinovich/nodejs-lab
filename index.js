cd712058e4a0ec7700cbb1161c8466bb9263e491
Veronika Martinovich <veranika.mart@gmail.com>
2021-10-03 15:57:35 +0300

    feat: create http server, create get-products and create-product endpoints

---
+++ index.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const products = require('./data');
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/products' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.write(JSON.stringify(products));
+    res.end();
+  } else if (req.url === '/products' && req.method === 'POST') {
+    let data = '';
+    req.on('data', (chunk) => {
+      data += chunk;
+    });
+    req.on('end', () => {
+      const newProduct = JSON.parse(data);
+      products.push(newProduct);
+      res.end();
+    });
+    res.writeHead(204, { 'Content-Type': 'application/json' });
+    res.write(JSON.stringify({ message: 'Created successfully' }));
+  } else {
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.write(JSON.stringify({ message: 'Route not found' }));
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
