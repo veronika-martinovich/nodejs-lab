51a9e3282b7434214ed593820b6ffaa1bb120720
Veronika Martinovich <veranika.mart@gmail.com>
2021-10-06 21:41:04 +0300

    feat: use express.js routing methods to process requests

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
-const http = require('http');
+const express = require('express');
 const products = require('./data');
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/products' && req.method === 'GET') {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.write(JSON.stringify(products));
+app.use(express.json());
+
+app
+  .route('/products')
+  .get((req, res) => {
+    res.status(200).json(products);
     res.end();
-  } else if (req.url === '/products' && req.method === 'POST') {
-    let data = '';
-    req.on('data', (chunk) => {
-      data += chunk;
-    });
-    req.on('end', () => {
-      const newProduct = JSON.parse(data);
-      products.push(newProduct);
-      res.writeHead(204, { 'Content-Type': 'application/json' });
-      res.write(JSON.stringify({ message: 'Created successfully' }));
-      res.end();
-    });
-  } else {
-    res.writeHead(404, { 'Content-Type': 'application/json' });
-    res.write(JSON.stringify({ message: 'Route not found' }));
+  })
+  .post((req, res) => {
+    products.push(req.body);
+    res.status(204).json(req.body);
     res.end();
-  }
+  });
+
+// Invalid request
+app.use((req, res) => {
+  res.json({
+    error: {
+      name: 'Error',
+      status: 404,
+      message: 'Invalid Request',
+      statusCode: 404,
+    },
+    message: 'Route not found',
+  });
 });
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
