b294ef44dab75524b99e996be6f6e55d4be295c8
Veronika Martinovich <veranika.mart@gmail.com>
2021-10-14 22:32:35 +0300

    feat: add types to schemas and models

--- src/db/product/db.ts
+++ src/db/product/db.ts
@@ -1,11 +1,11 @@
-const ProductModel = require('./model');
+const { Product, ProductModel } = require('./model');
 
 const getAll = async () => {
   const products = await ProductModel.find({});
   return products;
 };
 
-const save = async (product) => {
+const save = async (product: typeof Product) => {
   const newProduct = await ProductModel.create(product);
   return newProduct;
 };
