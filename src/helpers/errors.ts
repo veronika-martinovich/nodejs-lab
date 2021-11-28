e2d3769ccee5e580306095086c93f51af6e9b15d
Veronika Martinovich <veranika.mart@gmail.com>
2021-11-28 20:33:34 +0300

    feat: add not found error to db requests

--- src/helpers/errors.ts
+++ src/helpers/errors.ts
@@ -11,3 +11,15 @@ export class InvalidRequestError extends Error implements IError {
     this.message = message;
   }
 }
+
+export class NotFoundError extends Error implements IError {
+  statusCode: number;
+
+  message: string;
+
+  constructor(message: string) {
+    super();
+    this.statusCode = 404;
+    this.message = message;
+  }
+}
