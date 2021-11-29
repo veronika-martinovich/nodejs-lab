8081e4b609fa5d2991d34dabc23e1896ec52919b
Veronika Martinovich <veranika.mart@gmail.com>
2021-11-29 22:06:31 +0300

    refactor: validate query function

--- src/helpers/validate-query.ts
+++ src/helpers/validate-query.ts
@@ -3,43 +3,65 @@ import { IValidationParams } from '../types';
 import { InvalidRequestError } from './errors';
 
 export const validateQuery = (params: IValidationParams) => {
+  const FIELD_NAMES = {
+    DISPLAY_NAME: 'display name',
+    MIN_RATING: 'minimal rating',
+    CREATED_AT: 'creation date',
+    LIMIT: 'limit',
+    OFFSET: 'offset',
+    PRICE: 'prIce',
+    SORT_FIELD: 'sort field',
+    SORT_DIRECTION: 'sort direction',
+    CATEGORY_ID: 'category id',
+    INCLUDE_PRODUCTS: 'include products',
+    INCLUDE_TOP_3_PRODUCTS: 'include top 3 products',
+  };
+
+  const errorFields = [];
+
   if (params.displayName && !isString(params.displayName)) {
-    throw new InvalidRequestError('Invalid display name data');
+    errorFields.push(FIELD_NAMES.DISPLAY_NAME);
   }
   if (params.minRating && !isNumber(params.minRating)) {
-    throw new InvalidRequestError('Invalid minimal rating data');
+    errorFields.push(FIELD_NAMES.MIN_RATING);
   }
   if (params.createdAt && !isDate(params.createdAt)) {
-    throw new InvalidRequestError('Invalid created at data');
+    errorFields.push(FIELD_NAMES.CREATED_AT);
   }
   if (params.limit && !isNumber(params.limit)) {
-    throw new InvalidRequestError('Invalid limit data');
+    errorFields.push(FIELD_NAMES.LIMIT);
   }
   if (params.offset && !isNumber(params.offset)) {
-    throw new InvalidRequestError('Invalid offset data');
+    errorFields.push(FIELD_NAMES.OFFSET);
   }
   if (params.price) {
     const priceParts = params.price.split(':');
     priceParts.forEach((item) => {
       if (!isNumber(item)) {
-        throw new InvalidRequestError('Invalid price data');
+        errorFields.push(FIELD_NAMES.PRICE);
       }
     });
   }
   if (params.sortBy) {
     const sortParts = params.sortBy.split(':');
-
-    if (!isString(sortParts[0]) || !isSortOrder(sortParts[1])) {
-      throw new InvalidRequestError('Invalid sorting data');
+    if (!isString(sortParts[0])) {
+      errorFields.push(FIELD_NAMES.SORT_FIELD);
+    }
+    if (!isSortOrder(sortParts[1])) {
+      errorFields.push(FIELD_NAMES.SORT_DIRECTION);
     }
   }
   if (params.categoryId && !isString(params.categoryId)) {
-    throw new InvalidRequestError('Invalid category Id data');
+    errorFields.push(FIELD_NAMES.CATEGORY_ID);
   }
   if (params.includeProducts && !isBoolean(params.includeProducts)) {
-    throw new InvalidRequestError('Invalid include products data');
+    errorFields.push(FIELD_NAMES.INCLUDE_PRODUCTS);
   }
   if (params.includeTop3Products && !isBoolean(params.includeTop3Products)) {
-    throw new InvalidRequestError('Invalid include top 3 products data');
+    errorFields.push(FIELD_NAMES.INCLUDE_TOP_3_PRODUCTS);
+  }
+
+  if (errorFields.length) {
+    throw new InvalidRequestError(`Invalid data: ${errorFields.join(', ')}.`);
   }
 };
