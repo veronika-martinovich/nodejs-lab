1eec274252603ed0d82d42fb110941906d5bb9f8
Veronika Martinovich <veranika.mart@gmail.com>
2022-01-27 18:56:05 +0300

    feat: create admin endpoint tocreate products

--- src/resources/admin/product/admin.product.router.ts
+++ src/resources/admin/product/admin.product.router.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response, NextFunction } from 'express';
 import productsService from '../../product/product.service';
-// import { validateProductBody, validateProductQuery } from '../../product/product.validation';
+import { validateProductBody } from '../../product/product.validation';
 import { checkAdminRole } from '../../../helpers/check-admin-role';
 import { authenticate } from '../../../helpers/authenticate';
 
@@ -14,6 +14,24 @@ adminProductsRouter
       const productToReturn = await productsService.getById(productId);
 
       res.status(200).json(productToReturn);
+      res.end();
+    } catch (err) {
+      next(err);
+    }
+  });
+
+adminProductsRouter
+  .route('/')
+  .post(authenticate, checkAdminRole, validateProductBody, async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const newProduct = await productsService.save({
+        displayName: req.body.displayName,
+        totalRating: req.body.totalRating,
+        price: req.body.price,
+        categoryId: req.body.categoryId,
+      });
+
+      res.status(200).json(newProduct);
       res.end();
     } catch (err) {
       next(err);
