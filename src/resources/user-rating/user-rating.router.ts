e3f93c71fe113dfc13aac119f75b30a63e800c50
Veronika Martinovich <veranika.mart@gmail.com>
2022-02-07 12:57:39 +0300

    refactor: comments and commented code

--- src/resources/user-rating/user-rating.router.ts
+++ src/resources/user-rating/user-rating.router.ts
@@ -1,6 +1,5 @@
 import express, { Request, Response, NextFunction } from 'express';
 import userRatingsService from './user-rating.service';
-// import { io } from '../../index';
 
 const path = require('path');
 
@@ -9,7 +8,6 @@ const userRatingRouter = express.Router();
 userRatingRouter.route('/lastRatings').get(async (req: Request, res: Response, next: NextFunction) => {
   try {
     const lastRatings = await userRatingsService.getLastRatings();
-    // io.emit('newRating', { lastRatings });
     res.status(200).json(lastRatings);
     res.end();
   } catch (err) {
@@ -17,6 +15,7 @@ userRatingRouter.route('/lastRatings').get(async (req: Request, res: Response, n
   }
 });
 
+// Create enpoint for html page to not to create separate project for client side logic
 userRatingRouter.route('/lastRatingsPage').get(async (req: Request, res: Response, next: NextFunction) => {
   try {
     res.sendFile(path.join(__dirname, '../../screens/index.html'));
