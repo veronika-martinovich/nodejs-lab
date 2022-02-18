0b1f3ed588ec5da4f9810845c9c704f63e94950f
Veronika Martinovich <veranika.mart@gmail.com>
2022-02-18 19:27:40 +0300

    chore: adjust configs

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  transform: {
+    'node_modules/variables/.+\\.(j|t)sx?$': 'ts-jest',
+  },
+  transformIgnorePatterns: ['/node_modules/'],
 };
