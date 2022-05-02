611d3b49b8ffcf0ffe24b9b0f1f49b4f87c09029
Veronika Martinovich <veranika.mart@gmail.com>
2022-05-02 19:41:03 +0200

    feat: change port, add host for heroku deploy

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,13 @@ import { DBConnect } from './helpers/DBConnect';
 import { lastRatingsJob } from './jobs/last-ratings';
 import { server } from './index';
 
-const PORT = 3000;
+const port = process.env.PORT || 3000;
+const host = '0.0.0.0';
 
 DBConnect().then(() => {
   if (process.env.NODE_ENV !== 'test') {
-    server.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
+    server.listen(port, host, () => {
+      console.log(`Server is running on port ${port}`);
       lastRatingsJob.start();
     });
   }
